0f1d91d5a046e5d5d04b8a42655b9d44f6f50737
Ghadrr <gabrieldutraa1999@gmail.com>
2024-05-22 23:54:25 -0300

    aceita

--- services/bot.py
+++ services/bot.py
@@ -24,22 +24,19 @@ trainer.train([
     'obrigado',
     'fico feliz em ajudar, gostaria de mais alguma informação sobre o clima?',
 ])
-def preprocess_text_pt(text):
-    tokens = word_tokenize(text.lower(), language='portuguese')
-    stop_words = set(stopwords.words('portuguese'))
-    words = [word for word in tokens if word.isalnum() and word not in stop_words and word not in string.punctuation]
-    return " ".join(words)
-
 while True:
     request = input('you: ')
-    if request.lower() == 'sim':
+    if request.lower() in ['sim', 'claro', 'gostaria']:
         print('ok')
         request = input("informe a cidade que gostaria de saber o clima: ")
         pega_temp(request)
         print("Viezzi: gostaria de mais alguma informação sobre o clima?")
 
-    elif request.lower() in ['nao', 'não', 'sair ', 'Sair']:
+
+    elif request.lower() in ['nao', 'não', 'sair']:
+
         print('ok, tchau')
+
         break
     else:
         response = bot.get_response(request)
