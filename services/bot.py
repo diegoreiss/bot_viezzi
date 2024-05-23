a5e4bc29b05ce6d576ccc7f5e8375a65a6f5be90
Ghadrr <gabrieldutraa1999@gmail.com>
2024-05-23 01:27:35 -0300

    utils

--- services/bot.py
+++ services/bot.py
@@ -6,38 +6,48 @@ import string
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 
-from api import pega_temp
-
-bot = ChatBot('Bot')
-chatbot = ChatBot(
-    'Viezzi',
-    logic_adapters=[
-        'chatterbot.logic.BestMatch',
-        'chatterbot.logic.TimeLogicAdapter']
-)
-
-trainer = ListTrainer(bot)
-
-trainer.train([
-	'oi',
-    'ola, Seja bem vindo, me chamo Bot Viezzi, voce gostaria de saber alguma informação sobre o clima? ',
-    'obrigado',
-    'fico feliz em ajudar, gostaria de mais alguma informação sobre o clima?',
-])
-while True:
-    request = input('you: ')
-    if request.lower() in ['sim', 'claro', 'gostaria']:
-        print('ok')
-        request = input("informe a cidade que gostaria de saber o clima: ")
-        pega_temp(request)
-        print("Viezzi: gostaria de mais alguma informação sobre o clima?")
+from utlls import pega_temp
 
 
-    elif request.lower() in ['nao', 'não', 'sair']:
+def viezzi_init():
+    bot = ChatBot('Bot')
+    ChatBot(
+        'Viezzi',
+        logic_adapters=[
+            'chatterbot.logic.BestMatch',
+            'chatterbot.logic.TimeLogicAdapter']
+    )
 
-        print('ok, tchau')
+    trainer = ListTrainer(bot)
 
-        break
-    else:
-        response = bot.get_response(request)
-        print('Viezzi: ', response)
+    trainer.train([
+        'oi',
+        'ola, Seja bem vindo, me chamo Bot Viezzi, voce gostaria de saber alguma informação sobre o clima? ',
+        'obrigado',
+        'fico feliz em ajudar, gostaria de mais alguma informação sobre o clima?',
+    ])
+    while True:
+        request = input('you: ')
+        if request.lower() in ['sim', 'claro', 'gostaria', "clima"]:
+            print('ok')
+            request = input("informe a cidade que gostaria de saber o clima: ")
+            temp = pega_temp(request)
+            print(temp)
+            print("Viezzi: gostaria de mais alguma informação sobre o clima?")
+
+
+        elif request.lower() in ['nao', 'não', 'sair']:
+
+            print('ok, tchau')
+
+            break
+        elif request.lower() in ['porra', 'caralho', 'puta']:
+
+            print('Viezzi: não posso fornecer esse tipo de informação, mas você pode me fazer perguntas sobre o clima')
+            print("Viezzi: gostaria de alguma informação sobre o clima?")
+
+        else:
+            response = bot.get_response(request)
+            print('Viezzi: ', response)
+
+viezzi_init()
--- services/utlls.py
+++ services/utlls.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def pega_temp(cidade):
     api_key = "ccbff3a178263fe7bd130a13f0e90b8d"
@@ -13,11 +14,50 @@ def pega_temp(cidade):
             temperatura = data['main']['temp']
             humidade = data['main']['humidity']
 
-            ret =  f"\nCidade: {cidade}\nTemperatura: {temperatura-273:.2g}°C\nHumidade: {humidade}%.\n"
-            print(ret)
+            return f"\nCidade: {cidade}\nTemperatura: {temperatura-273:.2g}°C\nHumidade: {humidade}%.\n"
         else:
             return "Não foi possível obter informações sobre o clima."
     else:
         return "Erro ao consultar a API de previsão do tempo."
 
+def load_json():
+    with open ('treinamento.json', 'r', encoding = 'utf-8') as file:
+        conversations = json.load(file)
+    return conversations
 
+def json_train_bot(trainer):
+    conversations = load_json()
+    trainer.train(conversations)
+
+def export_json(bot, filename):
+    conversa = []
+
+    for msg in bot.storage.filter():
+        conversa.append({
+            'text': msg.text
+        })
+    with open(filename, 'w') as json_file:
+        json.dump(conversa, json_file, indent=4)
+
+def safe_chat(response):
+    p_proib = ['porra', 'caralho', 'puta']
+
+    for word in p_proib:
+        if word in response.lower():
+            return False
+        return True
+
+def safe_chat_response(bot, text):
+    response = chatbot_response(bot, text)
+
+    if safe_chat(response):
+        return response
+    else:
+        return('Viezzi: não posso fornecer esse tipo de informação, mas você pode me fazer perguntas sobre o clima')
+
+def chatbot_response(bot, text):
+    if text:
+        response = bot.get_response(text)
+        return response.text
+    else:
+        return "Viezzi: Não entendi, repita sua pergunta"
